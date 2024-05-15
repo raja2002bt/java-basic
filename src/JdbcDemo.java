966845b4ba8ac2b9575ef17030d2d1ebfd0f9321
Raja <144160739+raja2002bt@users.noreply.github.com>
2024-05-15 21:15:40 +0530

    Add files via upload

---
+++ src/JdbcDemo.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class JdbcDemo {
+    public static void main(String[] args) throws SQLException {
+
+        String sql ="select br_name  from branch where branch_id='3'";
+        String url ="jdbc:mysql://localhost:3306/branch";
+        String username ="root";
+        String password ="9159608502";
+
+
+        Connection con = DriverManager.getConnection(url,username,password);
+        Statement st=con.createStatement();
+        ResultSet rs=st.executeQuery(sql);
+        rs.next();
+        String name=rs.getString(1);
+        System.out.println(name);
+    }
+}
