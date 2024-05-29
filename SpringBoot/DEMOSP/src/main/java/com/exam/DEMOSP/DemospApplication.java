63dffa2e3a55f301adb6546a52537198c9b91fea
Raja <144160739+raja2002bt@users.noreply.github.com>
2024-05-29 22:59:31 +0530

    Add files via upload

---
+++ SpringBoot/DEMOSP/src/main/java/com/exam/DEMOSP/DemospApplication.java
@@ -0,0 +1,14 @@
+package com.exam.DEMOSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospApplication.class, args);
+		System.out.println("Hello>>");
+	}
+
+}
---
+++ SpringBoot/DEMOSP/src/test/java/com/exam/DEMOSP/DemospApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.DEMOSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
