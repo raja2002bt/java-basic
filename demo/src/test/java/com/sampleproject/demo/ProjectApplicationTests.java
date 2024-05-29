62037a4c850efa29921edf21582fc2facf92ad19
Raja <144160739+raja2002bt@users.noreply.github.com>
2024-05-29 21:43:50 +0530

    Add files via upload

---
+++ demo/src/test/java/com/sampleproject/demo/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
